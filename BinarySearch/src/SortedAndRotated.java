37cb295c9f368a6cd9d15338cef9c537d7024be7
ravi9409 <raviranjan9409@gmail.com>
2025-02-05 01:41:44 +0530

    modified

--- BinarySearch/src/SortedAndRotated.java
+++ BinarySearch/src/SortedAndRotated.java
@@ -1,21 +1,21 @@
 public class SortedAndRotated {
     public static void main(String[] args) {
-        int[] a = {1,2,3};
+        int[] a = {4,5,1,2,3};
         System.out.println(sortedAndRotated(a));
     }
 
     public static boolean sortedAndRotated(int[] nums) {
-        boolean flag = false;
-        int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1] || nums[i]+1 == nums[i+1]) {
-                    count++;
-                    if (count == 1)
-                        flag = true;
-            }else{
-                flag=false;
+        int c=0;
+        boolean flag=true;
+        for (int i=0;i<nums.length;i++){
+            if(nums[i]>nums[(i+1)%nums.length]){
+                c++;
+            }
+            if (c>1){
+                flag= false;
             }
         }
+
         return flag;
     }
 }
